f30169fff99c94a3402215ffe2378f03c9816c71
LêKhánhHòa <mcskhanhhoa@gmail.com>
2025-11-29 17:46:27 +0700

    fix: connect redis deploy render

--- src/config/db/redis.ts
+++ src/config/db/redis.ts
@@ -1,11 +1,11 @@
 import Redis from 'ioredis';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const redis = new Redis({
-    host: process.env.REDIS_HOST, // sử dụng host từ .env
-    port: Number(process.env.REDIS_PORT), // sử dụng port từ .env
-    password: process.env.REDIS_PASSWORD, // mật khẩu từ .env
-    tls: {}, // bắt buộc với Redis cloud
-    retryStrategy: time => Math.min(time * 50, 2000),
+    host: process.env.REDIS_HOST,
+    port: Number(process.env.REDIS_PORT),
 });
 
 redis.on('error', err => {
